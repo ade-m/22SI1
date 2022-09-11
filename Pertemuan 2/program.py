266056fdd2298647697069e39efe19277ef49361
ade-m <66313509+ade-m@users.noreply.github.com>
2022-09-11 22:38:44 +0700

    Initial commit

---
+++ Pertemuan 2/program.py
@@ -0,0 +1,12 @@
+# Program menghitung luas persegi
+
+#input panjang
+panjang = input('Silahkan input panjang :')
+#input lebar
+lebar = input('Silahkan input lebar : ')
+
+#luas = panjang x lebar
+luas = int(panjang) * int(lebar)
+
+print('Luas persegi adalah ')
+print(luas)
