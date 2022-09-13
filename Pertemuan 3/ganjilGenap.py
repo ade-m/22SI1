521e2b193ec0ec2d2b415c23d0c9d42b216d5f7b
yoedhie1990 <yudhistira.adhitya@gmail.com>
2022-09-13 11:34:20 +0700

    ganjil genap

---
+++ Pertemuan 3/ganjilGenap.py
@@ -0,0 +1,10 @@
+#program cek ganjil genap
+
+
+angka = input("masukkan angka = ")
+
+if(int(angka)%2==0):
+    print('bilangan ', angka, 'adalah genap')
+else:
+    print('bilangan ', angka, 'adalah ganjil')
+
