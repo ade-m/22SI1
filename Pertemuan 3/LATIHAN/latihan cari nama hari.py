74cb7341bf728bb80896df9e132b7fc14123ed6e
yoedhie1990 <yudhistira.adhitya@gmail.com>
2022-09-25 22:43:24 +0700

    latihan 3

---
+++ Pertemuan 3/LATIHAN/latihan cari nama hari.py
@@ -0,0 +1,20 @@
+#Farrel Alifah Raihan
+#03081220042
+#Menentukan Nama hari
+
+hari = input('Masukkan Unit hari : ')
+
+if(int(hari)==1):
+    print('hari ini adalah hari minggu')
+elif(int(hari)==2):
+    print('hari ini adalah hari senin')
+elif(int(hari)==3):
+    print('hari ini adalah hari selasa')
+elif(int(hari)==4):
+    print('hari ini adalah hari rabu')
+elif(int(hari)==5):
+    print('hari ini adalah hari kamis')
+elif(int(hari)==6):
+    print('hari ini adalah hari jumat')
+elif(int(hari)==7):
+    print('hari ini adalah hari sabtu')
