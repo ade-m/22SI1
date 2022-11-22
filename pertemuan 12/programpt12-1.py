e3894b433e890029bbe89ac5d264d760c28d5aeb
ade-m <66313509+ade-m@users.noreply.github.com>
2022-11-22 10:17:27 +0700

    pt 12

---
+++ pertemuan 12/programpt12-1.py
@@ -0,0 +1,22 @@
+
+def salam():
+    print("Halo, selamat pagi")
+
+def genap_apa_ganjil(bilangan):
+    if bilangan % 2 ==0 :
+        print("Genap")
+    else :
+        print("Ganjil")
+
+def luasPersegi(sisi):
+    luas = sisi*sisi
+    return luas
+
+salam()
+
+genap_apa_ganjil(10)
+genap_apa_ganjil(113)
+A = luasPersegi(10)
+print(luasPersegi(10))
+
+
